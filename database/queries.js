71c3126dbbeecd340bba2a810883a85209288f20
James McCormack <thejamesmccormack@gmail.com>
2017-10-11 16:34:06 -0700

    wrote schema and some db queries

--- database/queries.js
+++ database/queries.js
@@ -10,32 +10,36 @@ const db = pgp({
   database: databaseName,
 })
 
-function allCards() {
+const allCards = () => {
   return db.any("SELECT * FROM cards;")
 }
 
-function createCard({name, cost, game, attack, power, defense}) {
+const createCard = ({name, cost, game, attack, power, defense}) => {
   return db.one(`
     INSERT INTO cards (name, cost, game, attack, power, defense)
-    VALUES ($(name), $(cost), $(game), $(attack), $(power), $(defense))
+    VALUES ($1, $2, $3, $4, $5, $6)
     RETURNING cards.id;
     `, {name, cost, game, attack, power, defense})
 }
 
-function getCardById(id) {
-  return db.oneOrNone(`
-    SELECT * FROM cards
-    WHERE id = $1
-    `, id)
-}
-
-function cardsWithName(name) {
+const cardsFromSet = (...games) => {
   return db.any(`
     SELECT * FROM cards
-    WHERE name = $1;
-    `, name)
+    WHERE game=$1;`, [...games])
+}
+
+const createUser = (name, email, password) => {
+  return db.one(`
+    INSERT INTO users VALUES ($1, $2, $3) RETURNING *
+    `, [name, email, password])
+}
+
+const verifyUser = (email) => {
+  return db.one(
+    `SELECT * FROM users WHERE email=$1`, email
+  )
 }
 
 module.exports = {
-  db, allCards, createCard, getCardById, cardsWithName
+  db, allCards, createCard, cardsFromSet, createUser
 }
--- database/schema.sql
+++ database/schema.sql
@@ -1,9 +1,22 @@
 CREATE TABLE cards (
-  id SERIAL PRIMARY KEY,
+  card_id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   cost INTEGER NOT NULL,
   game VARCHAR(255),
   attack BOOLEAN,
   powerful BOOLEAN,
   defense BOOLEAN
+);
+
+CREATE TABLE users (
+  user_id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE sets (
+  set_id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(user_id),
+  sets TEXT
 );
